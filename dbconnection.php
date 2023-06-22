b2560646eaf6c1b6dfa84f98baf3b80a442cebca
kiophem <137384293+kiophem@users.noreply.github.com>
2023-06-22 15:48:04 +0800

    Create dbconnection.php

---
+++ dbconnection.php
@@ -0,0 +1,13 @@
+<?php
+    // Enter your host name, database username, password, and database name.
+    // If you have not set database password on localhost then set empty.
+    $con = mysqli_connect("localhost","root","","cert");
+
+    // Check connection
+    if (mysqli_connect_errno()){
+        echo "Failed to connect to MySQL: " . mysqli_connect_error();
+    }
+
+    date_default_timezone_set('Asia/Karachi');
+    $error="";
+?>
