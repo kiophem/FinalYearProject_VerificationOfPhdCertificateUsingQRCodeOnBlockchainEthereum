64ce74bfa91c29d789826bcb03bb790386cedb81
kiophem <137384293+kiophem@users.noreply.github.com>
2023-06-25 16:14:06 +0800

    Update addcertificate.php

--- addcertificate.php
+++ addcertificate.php
@@ -30,42 +30,30 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-<!--
-
-Template 2082 Pure Mix
-
-http://www.tooplate.com/view/2082-pure-mix
-
--->
+<!--Template 2082 Pure Mix http://www.tooplate.com/view/2082-pure-mix-->
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=Edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<meta name="keywords" content="">
 	<meta name="description" content="">
 
-	<!-- Site title
-   ================================================== -->
+	<!-- Site title================================================== -->
 	<title>Add Certificate</title>
 
-	<!-- Bootstrap CSS
-   ================================================== -->
+	<!-- Bootstrap CSS================================================== -->
 	<link rel="stylesheet" href="css/bootstrap.min.css">
 
-	<!-- Animate CSS
-   ================================================== -->
+	<!-- Animate CSS================================================== -->
 	<link rel="stylesheet" href="css/animate.min.css">
 
-	<!-- Font Icons CSS
-   ================================================== -->
+	<!-- Font Icons CSS================================================== -->
 	<link rel="stylesheet" href="css/font-awesome.min.css">
 	<link rel="stylesheet" href="css/ionicons.min.css">
 
-	<!-- Main CSS
-   ================================================== -->
+	<!-- Main CSS================================================== -->
 	<link rel="stylesheet" href="css/style.css">
 
-	<!-- Google web font 
-   ================================================== -->	
+	<!-- Google web font ================================================== -->	
   <link href='https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,700,300' rel='stylesheet' type='text/css'>
 
   <script src='node_modules/web3/dist/web3.min.js'></script>
@@ -73,8 +61,7 @@ http://www.tooplate.com/view/2082-pure-mix
 <body>
 
 
-<!-- Preloader section
-================================================== -->
+<!-- Preloader section================================================== -->
 <div class="preloader">
 
 	<div class="sk-spinner sk-spinner-pulse"></div>
@@ -180,8 +167,7 @@ http://www.tooplate.com/view/2082-pure-mix
    </div>
 </section>
 
-<!-- Footer section
-================================================== -->
+<!-- Footer section================================================== -->
 <footer>
 	<div class="container">
 		<div class="row">
